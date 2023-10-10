e14b82bd136e5e4b34ec17d27b54e7c14ea6327d
Pedro Daia <pedrodaia.c@gmail.com>
2023-10-10 14:04:24 -0300

    adding first styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<div class="header">
+  <a href="/">pedrodaia.me</a>
+  <a href="/">sobre mim</a>
+  <a href="/">projetos</a>
+  <a href="/">contato</a>
+</div>
+<slot />
+
+<style>
+  .header {
+    background-color: #c0c9ef;
+    height: 10vh;
+    margin-bottom: 10vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    gap: 3rem;
+    border: 1px solid #423913;
+  }
+  a {
+    color: #423913;
+    text-decoration: none;
+    font-size: 1.5rem;
+    align-self: center;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero-div">
+  <h1>Pedro Daia Cardoso</h1>
+  <p>Engenheiro de Software especializado em Web Design</p>
+</div>
+
+<style>
+  .hero-div {
+    width: 100%;
+    height: 90vh;
+    background-color: #c0c9ef;
+    margin-top: -10vh;
+  }
+  h1,
+  p {
+    color: #423913;
+  }
+</style>
