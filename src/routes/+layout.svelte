18962b95391b50fb8c7bc3924966d00dacb9b3bc
Pedro Daia <pedrodaia.c@gmail.com>
2023-10-10 15:43:27 -0300

    a few more positioning tweaks

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <div class="header">
-  <div class="avatar">
-    <img src="https://github.com/pxpc2.png" alt="pedro daia" />
-    <a href="/">pedrodaia.me</a>
+  <div class="header-content">
+    <div class="avatar">
+      <img src="https://github.com/pxpc2.png" alt="pedro daia" />
+      <a href="/">pedrodaia.me</a>
+    </div>
+    <a href="/">sobre mim</a>
+    <a href="/">portfolio</a>
+    <a href="/">contrate meus serviços</a>
   </div>
-  <a href="/">sobre mim</a>
-  <a href="/">portfolio</a>
-  <a href="/">contrate meus serviços</a>
 </div>
 <slot />
 
@@ -22,13 +24,19 @@
     height: 50%;
   }
   .header {
-    padding: 0 10rem 0 10rem;
+    padding: 1rem 10rem 0 10rem;
     background-color: #7656db;
-    height: 10vh;
+    gap: 3rem;
+  }
+  .header-content {
+    padding: 0 10rem 0 10rem;
+    background-color: rgb(122, 90, 228);
+    height: 7vh;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    gap: 3rem;
+    border: 1px solid #1a2900;
+    border-radius: 20rem;
   }
   a {
     color: #1a2900;
@@ -39,6 +47,6 @@
   a:hover {
     text-decoration: underline;
     text-decoration-color: #fffdfd;
-    text-underline-offset: 1rem;
+    text-underline-offset: 0.7rem;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,38 @@
-<div class="hero-div">
-  <div class="content">
-    <div class="text">
-      <h1 class="title">Sejam bem vindos ao meu mundo.</h1>
-      <p>
-        Sou Pedro Daia Cardoso, entusiasta por programação desde os 13 anos, e
-        hoje com 23 anos, sou engenheiro de software especialista em Web Design.
-      </p>
+<div class="frontpage">
+  <div class="hero-div">
+    <div class="content">
+      <div class="text">
+        <h1 class="title">Sejam bem vindos<br /> ao meu mundo.</h1>
+        <p>
+          Sou Pedro Daia Cardoso, entusiasta por programação desde os 13 anos de
+          idade,<br /> e hoje com 23 anos, sou Engenheiro de Software especialista
+          em Web Design.
+        </p>
+      </div>
+    </div>
+    <div class="decorations">
+      <img src="/athos.png" alt="Athos Bulcão" />
+      <img src="/athos.png" alt="Athos Bulcão" />
+      <img src="/athos.png" alt="Athos Bulcão" />
+      <img src="/athos.png" alt="Athos Bulcão" />
+      <img src="/athos.png" alt="Athos Bulcão" />
+      <img src="/athos.png" alt="Athos Bulcão" />
     </div>
   </div>
-  <div class="decorations">
-    <img src="/athos.png" alt="Athos Bulcão" />
-    <img src="/athos.png" alt="Athos Bulcão" />
-    <img src="/athos.png" alt="Athos Bulcão" />
-    <img src="/athos.png" alt="Athos Bulcão" />
-    <img src="/athos.png" alt="Athos Bulcão" />
-    <img src="/athos.png" alt="Athos Bulcão" />
+  <div class="about-div">
+    <h2>Olá mundo.</h2>
   </div>
 </div>
-<div class="about-div" />
 
 <style>
+  .frontpage {
+    overflow-y: hidden;
+  }
   .hero-div {
     display: flex;
     flex-direction: row;
     width: 100%;
-    height: 90vh;
+    height: 93vh;
     background-color: #7656db;
     position: relative;
     align-items: center;
@@ -34,22 +42,19 @@
     flex-direction: row;
     position: absolute;
     width: 100%;
-    bottom: 1rem;
+    bottom: 2rem;
     justify-content: space-between;
-    padding: 0 10rem 0 10rem;
     margin: 0 auto;
     overflow: hidden;
   }
   .decorations img {
-    width: 10rem;
+    width: 11rem;
   }
   .content {
     padding: 0rem 10rem 0rem 10rem;
   }
   .text {
-    width: 70vw;
-    max-width: 1300px;
-    margin-top: -10%;
+    margin-top: -27%;
   }
   .title {
     padding-bottom: 1rem;
@@ -57,8 +62,10 @@
   .about-div {
     width: 100%;
     height: 90vh;
-    background-color: black;
+    background-color: #480355;
     padding: 0rem 10rem 0rem 10rem;
+    display: flex;
+    align-items: center;
   }
   h1,
   p {
